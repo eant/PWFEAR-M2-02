e1ad12edceb4090d835ab0aa4c6db9574337578f
smessina <silvio.messina@eant.tech>
2019-05-04 13:27:39 -0300

    Simplificando componente React sin JSX

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ let hola = class Hola extends React.Component {
 
   render(){
     //React.createElement(ETIQUETA, ATRIBUTOS, CONTENIDO, PROPIEDADES DEL OBJETO)
-    let etiqueta = React.createElement("div", null, "Hola ", this.props.nombre)
-    return etiqueta
+    return React.createElement("div", null, "Hola ", this.props.nombre)
   }
 
 }
 ///////////////////////////////////////
 //ReactDOM.render(COMPONENTE, AREA)
-let app = document.querySelector("#app")
-let saludo = React.createElement(hola, { nombre : "Marte" })
 
-ReactDOM.render(saludo, app)
+ReactDOM.render(
+  React.createElement(hola, { nombre : "Luna" }),
+  document.querySelector("#app")
+)
