2f9ae86c3884868e61def250757302a5dc44e623
smessina <silvio.messina@eant.tech>
2019-05-18 11:08:58 -0300

    Refactorizando React con JS Nativo

--- Componentes-React/app.js
+++ Componentes-React/app.js
@@ -1,15 +1,27 @@
-let hola = class Hola extends React.Component {
+let Hola = class Hola extends React.Component {
 
   render(){
     //React.createElement(ETIQUETA, ATRIBUTOS, CONTENIDO, PROPIEDADES DEL OBJETO)
-    return React.createElement("div", null, "Hola ", this.props.nombre)
+    return React.createElement(
+      "div", //<-- Etiqueta
+      { style : { color : 'red', fontFamily : 'Arial' } }, //<-- Atributos
+      "Hola ", //<-- Contenido estatico
+      this.props.nombre //<-- Contenido dinamico (Propiedades del Objeto)
+    )
   }
 
 }
+let Imagen = class Imagen extends React.Component {
+  render(){
+
+      return React.createElement("img", { src : this.props.url, alt : this.props.name }, null)
+
+  }
+}
 ///////////////////////////////////////
 //ReactDOM.render(COMPONENTE, AREA)
-
 ReactDOM.render(
-  React.createElement(hola, { nombre : "Luna" }),
+  //React.createElement(Hola, { nombre : "Luna" }),
+  React.createElement(Imagen, { url : "https://lorempixel.com/100/100/animals", name : "Imagen al azar de animales" }),
   document.querySelector("#app")
 )
